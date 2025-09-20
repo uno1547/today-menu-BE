b7d5ca58ebc04b4b5f1adc2fdecd26f43de855bc
Yuno Kim <unokim1547@gmail.com>
2025-09-20 20:06:38 +0900

    add http

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const { Server } = require("socket.io");
 require('dotenv').config();
 
 const app = express();
-// const server = http.createServer(app); 이게 뭔가 문제가 있음
-const server = app.listen(3000, () => {
-  console.log('위쪽 app.listen')
-  console.log('Server is running on port 3000');
-});
+const server = http.createServer(app); // 이게 뭔가 문제가 있음
+// const server = app.listen(3000, () => {
+//   console.log('위쪽 app.listen')
+//   console.log('Server is running on port 3000');
+// });
 // CORS 설정 - 모든 도메인에서 요청 허용
 app.use(cors());
 app.use(express.json()); // JSON 바디 파싱 미들웨어
